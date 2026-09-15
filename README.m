4efd6ffd2c80f696dfcf4585ee3b416b2624686b
Sumedhb07 <badgujarsumedh@gmail.com>
2026-09-15 22:06:39 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,15 +1,27 @@
-# 💫 About Me:
-🎓 I'm a passionate **Computer Science student** interested in software development and technology.<br><br>🔭 I'm currently working on **Java and web development projects**.<br><br>🌱 I'm currently learning **Java, Data Structures & Algorithms, Web Development, and Git/GitHub**.<br><br>🤝 I'm looking to collaborate on **open-source projects, student projects, and innovative tech ideas**.<br><br>💡 I'm interested in **problem solving, programming, cybersecurity, and building useful applications**.<br><br>💬 Ask me about **Java, GitHub, programming, and project development**.<br><br>⚡ Fun fact: **I enjoy learning new technologies and turning ideas into working projects!**<br><br>🎯 My Goal: **To become a skilled software developer and build impactful technology.**
+💫 About Me:
+🎓 I’m a passionate Computer Science student interested in software development and technology.
+
+🔭 I’m currently working on Java and web development projects.
+
+🌱 I’m currently learning Java, Data Structures & Algorithms, Web Development, and Git/GitHub.
+
+🤝 I’m looking to collaborate on open-source projects, student projects, and innovative tech ideas.
+
+💡 I’m interested in problem solving, programming, cybersecurity, and building useful applications.
+
+💬 Ask me about Java, GitHub, programming, and project development.
+
+⚡ Fun fact: I enjoy learning new technologies and turning ideas into working projects!
+
+🎯 My Goal: To become a skilled software developer and build impactful technology.
+
+💻 Tech Stack:
+C C++ Java Python HTML5 JavaScript Oracle Angular.js Angular TailwindCSS MongoDB MySQL Canva Figma Sketch Matplotlib scikit-learn NumPy Pandas TensorFlow GitHub Meta
+
+📊 GitHub Stats:
 
 
-# 💻 Tech Stack:
-![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white) ![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white) ![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white) ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54) ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E) ![Oracle](https://img.shields.io/badge/Oracle-F80000?style=for-the-badge&logo=oracle&logoColor=white) ![Angular.js](https://img.shields.io/badge/angular.js-%23E23237.svg?style=for-the-badge&logo=angularjs&logoColor=white) ![Angular](https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white) ![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white) ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white) ![MySQL](https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white) ![Canva](https://img.shields.io/badge/Canva-%2300C4CC.svg?style=for-the-badge&logo=Canva&logoColor=white) ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![Sketch](https://img.shields.io/badge/Sketch-FFB387?style=for-the-badge&logo=sketch&logoColor=black) ![Matplotlib](https://img.shields.io/badge/Matplotlib-%23ffffff.svg?style=for-the-badge&logo=Matplotlib&logoColor=black) ![scikit-learn](https://img.shields.io/badge/scikit--learn-%23F7931E.svg?style=for-the-badge&logo=scikit-learn&logoColor=white) ![NumPy](https://img.shields.io/badge/numpy-%23013243.svg?style=for-the-badge&logo=numpy&logoColor=white) ![Pandas](https://img.shields.io/badge/pandas-%23150458.svg?style=for-the-badge&logo=pandas&logoColor=white) ![TensorFlow](https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=for-the-badge&logo=TensorFlow&logoColor=white) ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white) ![Meta](https://img.shields.io/badge/Meta-%230467DF.svg?style=for-the-badge&logo=Meta&logoColor=white)
-# 📊 GitHub Stats:
-![](https://github-readme-stats.shion.dev/api?username=sumedh_Badgujar&theme=dark&hide_border=false&include_all_commits=false&count_private=false)<br/>
-![](https://streak-stats.demolab.com/?user=sumedh_Badgujar&theme=dark&hide_border=false)<br/>
-![](https://github-readme-stats.shion.dev/api/top-langs/?username=sumedh_Badgujar&theme=dark&hide_border=false&include_all_commits=false&count_private=false&layout=compact)
 
----
-[![](https://komarev.com/ghpvc/?username=sumedh_Badgujar&icon=0&color=0)](https://visitcount.itsvg.in)
 
-<!-- Proudly created with GPRM ( https://gprm.itsvg.in ) -->
+
+
